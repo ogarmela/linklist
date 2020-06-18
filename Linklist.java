c19f6fcb50cddd6a64d43e30b4d1cf2390a7ba64
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-06-17 19:53:11 -0700

    Add files via upload

---
+++ Linklist.java
@@ -0,0 +1,22 @@
+
+public class Linklist {
+	public boolean isSorted() {
+	    ListNode pre = front;
+	    
+	    if (front == null) {
+	        return true;
+	    }
+	    
+	    while (pre.next != null) {
+	        ListNode current = pre.next;
+	        if (current.data < pre.data) {
+	            return false;
+	        }
+	        pre = current;
+	    }
+	    
+	    return true;
+	}
+
+
+}
